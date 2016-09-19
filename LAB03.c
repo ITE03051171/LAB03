81989c9ce1663298dbb23ebccd1b7b7914671ca0
Axcel <sena.t.9414@gmail.com>
2016-09-19 17:22:46 +0800

    dun use superuser,you noob

---
+++ LAB03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+printf("請輸入十個數字, 用空格隔開\n");
+scanf("%d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10);
+printf("您輸入的數字為:\n");
+printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",num1, num2, num3, num4, num5, num6, num7, num8, num9, num10);
+return 0;
+}
+
